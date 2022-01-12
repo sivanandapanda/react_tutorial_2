6352915d07d11bac211a147ea7f5b9ee46da4e66
siva <sivanandpanda@gmail.com>
2022-01-13 00:19:33 +0800

    react internals, React.memo(), useCallback, example of closure

---
+++ react-internals/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useCallback } from "react";
+
+import "./App.css";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState();
+
+  console.log("App is running");
+
+  //this ensures always same function object is reused when used
+  const toggleParagraphHandler = useCallback(() => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle paragraph</Button>
+    </div>
+  );
+}
+
+export default App;
