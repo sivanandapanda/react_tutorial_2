d51468a42a076acf1dbc8ccdb0e52efcdfee1b5f
siva <sivanandpanda@gmail.com>
2022-01-14 00:01:38 +0800

    class based equivalent and error boundary which is only available in class based components

---
+++ class-based-users-app/src/components/User.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import classes from "./User.module.css";
+
+export class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+/* const User = (props) => {
+  return <li className={classes.user}>{props.name}</li>;
+}; */
+
+export default User;
